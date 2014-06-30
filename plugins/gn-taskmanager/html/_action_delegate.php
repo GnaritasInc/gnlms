fb4c24fda389a3e096c23ce68d01187e67238344
David Santucci <dsantucci@gnaritas.com>
2014-06-30 11:01:09 -0400

    Added gn-calendar and gn-taskmanager submodules.

--- plugins/gn-taskmanager/html/_action_delegate.php
+++
@@ -1,7 +0,0 @@
-	<div>
-	<input type="radio" id="admin-action-delegate" name="admin-action" value="delegate" /><label for="admin-action-delegate">Claim Task or Delegate to another administrator:</label>
-	<select name="admin_id">
-	<option value="">Choose administrator</option>
-	<?php $ccnx_DataInterface->listOptions("administrator_user"); ?>
-	</select>
-	</div>
