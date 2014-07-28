259ae5ecaf2e779b7d1f18185d27ee569a4c6d7e
David Santucci <dsantucci@gnaritas.com>
2014-07-28 14:44:07 -0400

    Tweaks to available course list.

--- plugins/gn-lms/classes/templates/user-available-courses.php
+++ plugins/gn-lms/classes/templates/user-available-courses.php
@@ -1,11 +1,9 @@
+<?php if($records): ?>
 <ul class="gnlms-treeview">
 <?php foreach($records as $course): ?>
-		<li class="gnlms-course-title"><?php echo htmlspecialchars($course->title); ?>
-	<ul>
-		<li>Status: <?php echo $course->course_status ?></li>
-		<li><?php echo htmlspecialchars($course->description); ?></li>
-		<li><a class="gnlms-course-launch" href="/course-monitor/?id=<?php echo $course->id; ?>">Launch course</a></li>
-	</ul>
-	</li>
+	<li class="gnlms-course-title"><a href="/course-detail/?id=<?php echo $course->id; ?>"><?php echo htmlspecialchars($course->title); ?></a></li>
 <?php endforeach; ?>
 </ul>
+<?php else: ?>
+	<p>No available courses.</p>
+<?php endif; ?>
