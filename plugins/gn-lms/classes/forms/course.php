9d4492c00331b41c47a3b8a8b8dd5a526e3c7bba
David Santucci <dsantucci@gnaritas.com>
2015-02-27 13:02:12 -0500

    Fixed course status text date format.

--- plugins/gn-lms/classes/forms/course.php
+++ plugins/gn-lms/classes/forms/course.php
@@ -19,13 +19,13 @@
 		switch($registration->course_status) {
 			case "Registered":
 			case "In Progress":
-				$statusText = "You registered for this course on ".date('F n, Y', strtotime($registration->registration_date)).'. <a class="gnlms-course-launch gnlms-button" href="'.$this->getCourseLaunchURL($id).'">Launch course</a>';
+				$statusText = "You registered for this course on ".date('F j, Y', strtotime($registration->registration_date)).'. <a class="gnlms-course-launch gnlms-button" href="'.$this->getCourseLaunchURL($id).'">Launch course</a>';
 				break;
 			case "Expired":
-				$statusText = "Your registration for this course expired on ".date('F n, Y', strtotime($registration->expiration_date)).".";
+				$statusText = "Your registration for this course expired on ".date('F j, Y', strtotime($registration->expiration_date)).".";
 				break;
 			case "Completed":
-				$statusText = "You completed this course on ".date('F n, Y', strtotime($registration->course_completion_date)).".";
+				$statusText = "You completed this course on ".date('F j, Y', strtotime($registration->course_completion_date)).".";
 				break;
 		}
 	}
