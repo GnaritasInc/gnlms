caefa0da83b53cf30baaa9c84ccf9762e310b3e5
cwisdo <cw2@gnaritas.com>
2015-02-27 16:43:47 -0500

    Registration/Profile Updates

--- plugins/gn-lms/scripts/gnlms.js
+++ plugins/gn-lms/scripts/gnlms.js
@@ -1,5 +1,20 @@
 
+function gnlms_css_hacks($) {
+
+	$("#setupform label[for='user_name']").hide();
+	$("#setupform input[name='user_name']").attr("type","hidden");
+	$("#setupform input[name='user_name']").nextAll("span.hint").hide(); //attr("type","hidden");
+
+	$("#setupform input[name='user_email']").on("input",function() {
+			$("#setupform input[name='user_name']").val($(this).val());
+			});
+
+}
+
+
 jQuery(document).ready(function ($) {
+	gnlms_css_hacks($);
+
 	$("div.gnlms-form-dialog").each(function () {
 		var width = $(this).attr("data-width") ? parseInt($(this).attr("data-width")) : 500;
 		var height = $(this).attr("data-height") ? parseInt($(this).attr("data-height")) : 300;
@@ -46,9 +61,9 @@ jQuery(document).ready(function ($) {
 			"height": height
 		});
 	});
-	
+
 	$("#gnlms-sc-dialog").dialog("option", {"buttons":{"OK": function () { $(this).dialog("close"); }}});
-	
+
 	$("#gnlms-sc-dialog form.shopping_cart_update").submit(function (e) {
 		e.preventDefault();
 		ajaxFormSubmit.call(this, function (data, textStatus, xhr) {
@@ -67,12 +82,12 @@ jQuery(document).ready(function ($) {
 				});
 			}
 		});
-		
+
 		return false;
 	});
-	
 
-	
+
+
 	$("a.gnlms-open-dialog").click(function (event) {
 		var myDialog = $("#"+$(this).attr("data-dialogid"));
 		if(myDialog.length) {
@@ -81,16 +96,16 @@ jQuery(document).ready(function ($) {
 			return false;
 		}
 	});
-	
+
 	$("a.gnlms-open-dialog:not(#menu a)").button();
-	
+
 	function ajaxFormSubmit(complete) {
 		complete = complete || ajaxPostComplete;
-		$.post(gnlms.ajaxurl, $(this).serialize(), complete, "json");	
+		$.post(gnlms.ajaxurl, $(this).serialize(), complete, "json");
 	}
-	
-	
-	
+
+
+
 	function ajaxPostComplete (data, textStatus, xhr) {
 		if(data.status != "OK") {
 			reportAjaxError(data.msg);
@@ -98,19 +113,19 @@ jQuery(document).ready(function ($) {
 		else {
 			$(this).parent("div.gnlms-form-dialog").dialog("close");
 			location.reload(true);
-		}		
+		}
 	}
-	
+
 	function reportAjaxError (msg) {
 		displayError(msg);
 	}
-	
-	
-	$("form#gnlms-course-user-selection").submit(function () {		
-		$("#gnlms-found-users").load(gnlms.ajaxurl, $(this).serialize());		
-		return false;		
+
+
+	$("form#gnlms-course-user-selection").submit(function () {
+		$("#gnlms-found-users").load(gnlms.ajaxurl, $(this).serialize());
+		return false;
 	});
-	
+
 	$("#gnlms-found-users").on("click", "input:checkbox.gn-select-all", function () {
 		var checked = this.checked;
 		var name = $(this).attr("data-name");
@@ -118,9 +133,9 @@ jQuery(document).ready(function ($) {
 			this.checked = checked;
 		});
 	});
-	
+
 	$("a.gnlms-button").button();
-	
+
 	$("a.gnlms-open-dialog-form").click(function (event) {
 		var dialogSelector = "#"+$(this).attr("data-dialogid");
 		var queryParams = getParams();
@@ -141,31 +156,31 @@ jQuery(document).ready(function ($) {
 			dialogTitle = dialogTitle.replace(/^Edit /i, "Create ");
 			$(dialogSelector).dialog("option", "title", dialogTitle);
 		}
-		
+
 		$(dialogSelector).load(gnlms.ajaxurl, $.param(data), function () {
 			setDatePickers();
 			$(dialogSelector).dialog("open");
-			
+
 		});
-		
+
 		event.preventDefault();
 		return false;
-		
+
 	});
-	
+
 	$("a.gnlms-course-launch").click (function(event) {
-	
+
 		if ($("#gnlms-course-monitor").length) {
-		
+
 			$("#gnlms-course-monitor").remove();
 		}
-		
+
 		if (!$("#gnlms-course-monitor").length) {
 				$("<div title='Course Monitor' id='gnlms-course-monitor'></div>").appendTo("body")
-				.load($(this).attr("href"), 
-				
+				.load($(this).attr("href"),
+
 					function (responseText, textStatus, XMLHttpRequest) {
-						
+
 						jQuery("#gnlms-course-monitor").dialog(
 						{
 						"modal":true,
@@ -173,8 +188,8 @@ jQuery(document).ready(function ($) {
 						});
 						launchCourse(courseURL);
 					}
-						
-					);		
+
+					);
 		}
 		/*
 		IE no like re-use
@@ -185,58 +200,58 @@ jQuery(document).ready(function ($) {
 
 				jQuery("#gnlms-course-monitor").dialog("open");
 				launchCourse(courseURL);
-				
+
 
 			});
 		}
 		*/
-		
-		
+
+
 		//$(myDialog).dialog("open");
-		
+
 		event.preventDefault();
 		return false;
-		
+
 	});
-	
-	
-	
+
+
+
 	$("a.gnlms-announcement-delete").click(function (event) {
 		if(!confirm("Delete this announcement?")) {
 			return false;
 		}
-		
+
 		var data = {
 			"id":$(this).attr("data-id"),
 			"action":"gnlms-delete-announcement",
 			"gnlms_nonce":gnlms.nonce
 		};
 		$.post(gnlms.ajaxurl, data, ajaxPostComplete, "json");
-		
+
 		event.preventDefault();
 		return false;
 	});
-	
+
 	$("#gnlms-course-user-assignment").on("dialogopen", function () {
 		$("#gnlms-course-user-selection").get(0).reset();
 		$("#gnlms-found-users").empty();
 		var buttons = $(this).dialog("option", "buttons");
 		buttons[0].text="Register Users";
 		buttons[0].disabled = true;
-		
+
 		$(this).dialog("option", "buttons", buttons);
-		
+
 	});
-	
-	$("#gnlms-course-user-assignment").on("click", function () {		
+
+	$("#gnlms-course-user-assignment").on("click", function () {
 		var buttons = $(this).dialog("option", "buttons");
 		var checked = $(this).find("input:checked").length ? true : false;
-		
+
 		buttons[0].disabled = !checked;
-		
+
 		$(this).dialog("option", "buttons", buttons);
 	});
-	
+
 });
 
 
