a3a4dcfce7534f87bd902651abe1bb12d704567e
David Santucci <dsantucci@gnaritas.com>
2015-03-03 10:20:04 -0500

    Modified shopping cart portlet to hide when empty.

--- plugins/gn-lms/classes/templates/shopping-cart.php
+++ plugins/gn-lms/classes/templates/shopping-cart.php
@@ -1,5 +1,5 @@
 <h2><?php echo $atts['title']; ?></h2>
-<div class="full_span">
+<div class="full_span<?php echo count($context) ? "" : " gn-empty"?>">
 <?php if($this->err): ?><p class="gnlms-msg gnlms-err"><?php echo $this->err; ?></p><?php endif; ?>
 <?php include("_shopping_cart_content.php"); ?>
 
