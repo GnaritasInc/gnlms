3b36eef034145ea5a1beda1cc8af483246b7575d
David Santucci <dsantucci@gnaritas.com>
2015-03-18 12:38:41 -0400

    Removed calls to gnlms_LMS::getSiteURL()

--- plugins/gn-lms/classes/forms/user.php
+++ plugins/gn-lms/classes/forms/user.php
@@ -8,12 +8,12 @@
 		<?php endif; ?>
 	</div>
 	<input type="hidden" name="gnlms_data_form" value="user"/>
-	<input type="hidden" name="_redirect" value="<?php echo $this->getSiteURL("/users/"); ?>" />
+	<input type="hidden" name="_redirect" value="/users/" />
 	<?php if($context['id']): ?>
 		<input type="hidden" name="id" value="<?php echo($context['id']); ?>" />
 	<?php endif; ?>
 
 	<?php include("_user_fields.php");?>
 
-	<input type="submit" value="<?php echo $context['id'] ? "Update" : "Add" ?> User"/>
+	<input type="submit" value="Submit"/>
 </form>
